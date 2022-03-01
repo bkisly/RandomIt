0cbadd4f474ec5635f5e7b0d032628e543b75012
Bartosz Kisły <b.kisly@gmail.com>
2022-03-01 16:25:12 +0100

    Reorganized view template. Added About element

--- RandomIt/RandomIt/AppShell.xaml.cs
+++ RandomIt/RandomIt/AppShell.xaml.cs
@@ -10,13 +10,12 @@ namespace RandomIt
         public AppShell()
         {
             InitializeComponent();
+            Shell.SetTabBarIsVisible(this, false);
         }
 
-        /*
-        private async void OnMenuItemClicked(object sender, EventArgs e)
+        private async void About_Clicked(object sender, EventArgs e)
         {
-            await Shell.Current.GoToAsync("//LoginPage");
+            await Shell.Current.GoToAsync("//AboutPage");
         }
-        */
     }
 }
