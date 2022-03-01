63f8b745ca18ea25f453e601664956752cf22abc
Bartosz Kisły <b.kisly@gmail.com>
2022-03-01 17:02:12 +0100

    Implemented navigation template

--- RandomIt/RandomIt/AppShell.xaml.cs
+++ RandomIt/RandomIt/AppShell.xaml.cs
@@ -1,4 +1,4 @@
-﻿// using RandomIt.Views;
+﻿using RandomIt.Views;
 using System;
 using System.Collections.Generic;
 using Xamarin.Forms;
--- RandomIt/RandomIt/Views/AboutPage.xaml.cs
+++ RandomIt/RandomIt/Views/AboutPage.xaml.cs
@@ -16,5 +16,10 @@ namespace RandomIt.Views
         {
             InitializeComponent();
         }
+
+        private async void BackButton_Clicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync($"//{nameof(RandomNumberPage)}");
+        }
     }
 }
