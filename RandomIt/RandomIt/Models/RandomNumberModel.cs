eab29d74042eae73af7d769c9a6defb4e8bb388b
Bartosz Kisły <b.kisly@gmail.com>
2022-03-07 15:18:37 +0100

    Created random number model

---
+++ RandomIt/RandomIt/Models/RandomNumberModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RandomIt.Models
+{
+    internal class RandomNumberModel
+    {
+        public double MinValue { get; set; }
+        public double MaxValue { get; set; }
+        public int Precision { get; set; }
+
+        private Random _random;
+
+        public RandomNumberModel()
+        {
+            _random = new Random();
+        }
+
+        public int GenerateRandomInt()
+        {
+            return _random.Next((int)MinValue, (int)MaxValue + 1);
+        }
+
+        public double GenerateRandomDouble()
+        {
+            int min = (int)MinValue * Precision;
+            int max = (int)MaxValue * Precision;
+            return (double)_random.Next(min, max) / (double)Precision;
+        }
+    }
+}
