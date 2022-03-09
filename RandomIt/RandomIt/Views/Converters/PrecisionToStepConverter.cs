5997782a39460a0d8bbd0da0504d35cf4e2e6e26
Bartosz Kisły <b.kisly@gmail.com>
2022-03-09 14:50:50 +0100

    Added precision converter

---
+++ RandomIt/RandomIt/Views/Converters/PrecisionToStepConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace RandomIt.Views.Converters
+{
+    internal class PrecisionToStepConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is int)
+            {
+                return 1 / Math.Pow(10, (int)value);
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
