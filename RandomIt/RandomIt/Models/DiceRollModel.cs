bcb6597f10d2e5b1d89d2b3aade669d5f1120562
Bartosz Kisły <b.kisly@gmail.com>
2022-03-24 20:12:55 +0100

    Implemented dice roll model

---
+++ RandomIt/RandomIt/Models/DiceRollModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RandomIt.Models
+{
+    internal class DiceRollModel
+    {
+        private readonly Random _random;
+
+        public int DiceAmount { get; set; }
+
+        public DiceRollModel()
+        {
+            _random = new Random();
+            DiceAmount = 1;
+        }
+
+        public IEnumerable<int> Roll()
+        {
+            List<int> rollResults = new List<int>();
+
+            for (int i = 0; i < DiceAmount; i++)
+                rollResults.Add(_random.Next(6) + 1);
+
+            return rollResults;
+        }
+    }
+}
