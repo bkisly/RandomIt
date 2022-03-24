026fbdbf39d958476b6c00a2e7bf3d617f5fd738
Bartosz Kisły <b.kisly@gmail.com>
2022-03-24 20:43:24 +0100

    Created view model for dice roll, modified architecture

--- RandomIt/RandomIt/Models/DiceRollModel.cs
+++ RandomIt/RandomIt/Models/DiceRollModel.cs
@@ -4,26 +4,9 @@ using System.Text;
 
 namespace RandomIt.Models
 {
-    internal class DiceRollModel
+    internal class DiceRollModel : ThrowableModel
     {
-        private readonly Random _random;
-
-        public int DiceAmount { get; set; }
-
-        public DiceRollModel()
-        {
-            _random = new Random();
-            DiceAmount = 1;
-        }
-
-        public IEnumerable<int> Roll()
-        {
-            List<int> rollResults = new List<int>();
-
-            for (int i = 0; i < DiceAmount; i++)
-                rollResults.Add(_random.Next(6) + 1);
-
-            return rollResults;
-        }
+        public DiceRollModel() : base(6)
+        { }
     }
 }
---
+++ RandomIt/RandomIt/ViewModels/DiceRollViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RandomIt.Models;
+
+namespace RandomIt.ViewModels
+{
+    internal class DiceRollViewModel : ThrowableViewModel
+    {
+        public DiceRollViewModel() : base(new DiceRollModel())
+        { }
+    }
+}
