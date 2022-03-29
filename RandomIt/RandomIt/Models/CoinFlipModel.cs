4140989bb2356944d066fccca31bb066c7a8e1cd
Bartosz Kisły <b.kisly@gmail.com>
2022-03-29 13:29:53 +0200

    Created model and view model for coin flip

---
+++ RandomIt/RandomIt/Models/CoinFlipModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RandomIt.Models
+{
+    internal class CoinFlipModel : ThrowableModel
+    {
+        public CoinFlipModel() : base(2) 
+        { }
+    }
+}
